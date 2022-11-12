c6345091ae4a6284a5fd3d29cd746355b8519ed1
hannahBak <gosk031240@gmail.com>
2022-11-12 20:09:22 +0900

    생명 ui추가

---
+++ ui.py
@@ -0,0 +1,31 @@
+from pico2d import *
+import random
+import game_world
+import game_framework
+list = []
+
+from coin import Coin
+
+class Life:
+    image = None
+    def __init__(self):
+        if Life.image == None:
+            Life.image = load_image('heart.png')
+        self.y = 480
+        global list
+        self.x = random.choice([20, 50, 80])
+        while self.x in list:
+            self.x = random.choice([20, 50, 80])
+        list.append(self.x)
+
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+    def update(self):
+        pass
+
+
+
+
