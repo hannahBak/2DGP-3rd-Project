be70c210c4d93e5a03da8d3d55b0b7b9bbd086ab
hannahBak <gosk031240@gmail.com>
2022-12-05 23:39:58 +0900

    퍼즈 기능 추가 및 아이콘 수정

--- pause_state.py
+++ pause_state.py
@@ -1,12 +1,13 @@
 import game_framework
 from pico2d import *
 import play_state
+import title
 
 image = None
 
 def enter():
     global image
-    image = load_image('resource\\heart.png')
+    image = load_image('resource\\pause_.png')
 
 def exit():
     global image
@@ -20,13 +21,27 @@ def draw():
 
 def handle_events():
     events = get_events()
+    number = 1
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN:
-            match event.key:
-                case pico2d.SDLK_1:
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP):
+            if number == 2:
+                number -= 1
+                print(number)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_DOWN):
+            if number == 1:
+                number += 1
+                print(number)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RETURN):
+            match number:
+                case 1:
                     game_framework.pop_state()
+                case 2:
+                    game_framework.change_state(title)
+
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
 
 
 def update():
